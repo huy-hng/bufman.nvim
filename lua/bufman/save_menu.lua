cea81aeb464ea741d840c4a3961c94a5f0caceeb
Huy <huy@hoang.dev>
2023-12-11 16:35:58 +0100

    dont encode state, reorder bufferlist without bugs

--- lua/bufman/save_menu.lua
+++ lua/bufman/save_menu.lua
@@ -3,15 +3,17 @@ local list_manager = require('bufman.list_manager')
 local M = {}
 
 function M.decode_state(encoded)
-	local state = vim.json.decode(encoded)
+	-- local state = vim.json.decode(encoded)
+	local state = encoded
 	if not state then return end
 	---@type buffer_list
 	local buffer_list = state.buffer_list
 
-	-- P(buffer_list)
-	list_manager.buffer_list = buffer_list
-	list_manager.add_buffers()
+	-- list_manager.buffer_list = buffer_list
+	-- list_manager.add_buffers()
+	list_manager.sync_buffer_list()
 
+	-- put all buffers in the order
 	for i, buf in ipairs(buffer_list) do
 		for j, b in ipairs(list_manager.buffer_list) do
 			if b.filename == buf.filename then
@@ -20,14 +22,10 @@ function M.decode_state(encoded)
 			end
 		end
 	end
-	-- P(list_manager.buffer_list)
 end
 
-function M.encode_state()
-	local state = {
-		buffer_list = list_manager.buffer_list,
-	}
-	return vim.json.encode(state)
+function M.encode_state() --
+	return vim.json.encode { buffer_list = list_manager.buffer_list }
 end
 
 return M
