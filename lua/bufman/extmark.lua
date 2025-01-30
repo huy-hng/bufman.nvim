7466a48e49c894665d4f8995ace34d54aaac2938
Huy <huy@hoang.dev>
2025-01-30 14:45:55 +0100

    .

--- lua/bufman/extmark.lua
+++ lua/bufman/extmark.lua
@@ -23,13 +23,4 @@ function M.set_extmark(bufnr, row, content, extra_opts)
 	vim.api.nvim_buf_set_extmark(bufnr, ns, row, 0, mark_opts)
 end
 
-function M.set_filenames(bufman_bufnr, buffer_list)
-	for i, bufnr in pairs(buffer_list) do
-		local bufname = vim.api.nvim_buf_get_name(bufnr)
-		local fname = filename.get_filename(bufname)
-		M.set_extmark(bufman_bufnr, i - 1, { { tostring(bufnr) .. ' ' }, { fname } })
-	end
-
-end
-
 return M
